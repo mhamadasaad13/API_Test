06535baa50e004aae142ece2612138b049d3971c
mhamadasaad13 <asaadmhamad316@gmail.com>
2024-07-17 00:39:45 +0300

    third commit

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ const xlsx = require('xlsx');
 let wb = xlsx.readFile("Data.xlsx");
 let ws = wb.Sheets["Employee"];
 let data = xlsx.utils.sheet_to_json(ws);
+const PORT = process.env.PORT || 3030
 app.get('/', function (req, res) {
   res.send(data);
 })
 
     
-app.listen(3000);
+app.listen(PORT);
