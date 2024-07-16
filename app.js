9c48db9a47e99d727d863a0332968fa5d774afe6
mhamadasaad13 <asaadmhamad316@gmail.com>
2024-07-16 23:21:44 +0300

    second commit

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const app = express();
 const cors = require('cors');
 app.use(cors());
 const xlsx = require('xlsx');
-let wb = xlsx.readFile("../Data.xlsx");
+let wb = xlsx.readFile("Data.xlsx");
 let ws = wb.Sheets["Employee"];
 let data = xlsx.utils.sheet_to_json(ws);
 app.get('/', function (req, res) {
   res.send(data);
 })
+
     
 app.listen(3000);
